3d53108a12aa929b7c01f7f68937cefb38e0a522
Arta Seyedian <arta.seyedian@gmail.com>
2024-07-09 00:44:55 -0400

    finally got transparent philly borders

--- src/script.js
+++ src/script.js
@@ -1,6 +1,22 @@
 const width = 960;
 const height = 600;
 
+let currentSlide = 0;
+const slides = [
+    { title: "Overview", content: "" },
+    { title: "Fatality Hotspots", content: "" },
+    { title: "Time of Day Analysis", content: "" },
+    { title: "Vehicle Types Involved", content: "" },
+    { title: "Demographic Insights", content: "" }
+];
+
+// let phillySVG = d3.xml('/assets/Philadelphia (1).svg')
+let phillyMap = d3.json("/assets/geojson.json")
+// let phillyMap = d3.json("https://raw.githubusercontent.com/blackmad/neighborhoods/master/philadelphia.geojson")
+let phillyCSV = d3.csv("/assets/philly_crashes.csv")
+
+// const boundingBox = phillyMap
+
 const projection = d3.geoMercator()
     .center([-75.1652, 39.9526])  // Center on Philadelphia
     .scale(70000)
@@ -8,67 +24,28 @@ const projection = d3.geoMercator()
 
 const path = d3.geoPath().projection(projection);
 
-// let phillySVG = d3.xml('/assets/Philadelphia (1).svg')
-let phillyMap = d3.json("/assets/geojson.json")
-// let phillyMap = d3.json("https://raw.githubusercontent.com/blackmad/neighborhoods/master/philadelphia.geojson")
-let phillyCSV = d3.csv("/assets/philly_crashes.csv")
+Promise.all([
+    d3.xml('/assets/Philadelphia (1).svg'),
+    phillyMap,
+    d3.csv("/assets/philly_crashes.csv")
+]).then(([svgData, phillyMap, crashData]) => {
+    const svg = d3.select("#visualization").append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-// const boundingBox = phillyMap.boundingbox;
-// const projection = d3.geoMercator()
-//     .fitExtent([[0, 0], [width, height]], {
-//         type: "Polygon",
-//         coordinates: [[
-//             [boundingBox[2], boundingBox[0]],
-//             [boundingBox[3], boundingBox[0]],
-//             [boundingBox[3], boundingBox[1]],
-//             [boundingBox[2], boundingBox[1]],
-//             [boundingBox[2], boundingBox[0]]
-//         ]]
-//     });
+    // Append the loaded SVG to the container
+    const importedSVG = d3.select(svgData.documentElement);
+    svg.node().appendChild(importedSVG.node());
 
-Promise.all([phillySVG, phillyMap, phillyCSV])
-    .then(([svgData, phillyMap, crashData]) => {
-        const svg = d3.select("#visualization").append("svg")
-            .attr("width", width)
-            .attr("height", height);
+    // Draw the Philadelphia boundaries on top of the SVG
+    svg.selectAll(".boundary")
+        .data([phillyMap.geojson])
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("fill", "none")  // This makes it transparent
+        .attr("stroke", "black")
+        .attr("stroke-width", 2)
+        .raise(); // Bring phillyMap to the front
 
-        if (svgData) {
-            // Append the loaded SVG to the container
-            const importedSVG = d3.select(svgData.documentElement);
-            svg.node().appendChild(importedSVG.node());
-        } else {
-            console.log("svgData is empty");
-        }
-
-        console.log(phillyMap.geojson)
-        svg.selectAll(".boundary")
-            .data([phillyMap])
-            .enter()
-            .append("path")
-            .attr("d", path)
-            .attr("fill", "none")
-            .attr("stroke", "black")
-            .attr("stroke-width", 2);
-        // Extract and append the entire SVG content
-        // const importedSVG = d3.select(svgData.documentElement);
-        // const svgContent = importedSVG.html();
-
-        // svg.append("g")
-        //     .attr("class", "philly-roads")
-        //     .html(svgContent);
-
-        // Style or modify specific elements if needed
-        // svg.select("#Land").remove();  // Remove the land layer if you don't need it
-        // svg.selectAll(".st1")  // Assuming .st1 is the class for primary roads
-        //     .attr("stroke", "#4788c8")
-        //     .attr("stroke-width", 1);
-
-
-        // svg.selectAll("path").attr("fill", d => {
-        //     console.log(d + "this is d")
-        //     const zipCode = d.properties.ZIP_CODE;  // Assuming ZIP_CODE is a property of your GeoJSON data
-        //     return colorScale(fatalitiesByZip.get(zipCode) || 0);
-        //   });
-
-
-    });
+});
