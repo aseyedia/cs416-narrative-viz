86a65f8e008a1a3d6520f5ff840d3acb1be508c9
Arta Seyedian <arta.seyedian@gmail.com>
2024-07-09 01:14:39 -0400

    refactor: Update Philadelphia traffic fatalities visualization
    - Improve code readability and maintainability
    - Adjust map projection center and translation
    - Load SVG and GeoJSON data asynchronously
    - Style and modify specific elements of the SVG

--- src/script.js
+++ src/script.js
@@ -1,13 +1,13 @@
-const width = 960*2;
-const height = 600*2;
+const width = 960 * 2;
+const height = 600 * 2;
 
-let phillyMap = d3.json("/assets/phillyRoads.json")
-let phillyCSV = d3.csv("/assets/philly_crashes.csv")
+let phillyMap = d3.json("/assets/phillyRoads.json");
+let phillyCSV = d3.csv("/assets/philly_crashes.csv");
 
 const projection = d3.geoMercator()
-    .center([-75.1652, 39.9526]) 
+    .center([-75.1652, 39.9526])
     .scale(70000)
-    .translate([width / 2, height / 1.6]);
+    .translate([width / 2, height / 2]);
 
 const path = d3.geoPath().projection(projection);
 
@@ -18,9 +18,13 @@ Promise.all([
 
     const svg = d3.select("#visualization").append("svg")
         .attr("width", width)
-        .attr("height", height);
+        .attr("height", height)
+        .call(d3.zoom().on("zoom", function (event) {
+            svg.attr("transform", event.transform);
+        }))
+        .append("g");
 
-    console.log(phillyMap)
+    console.log(phillyMap);
 
     svg.selectAll("path")
         .data(phillyMap.features)
@@ -28,11 +32,22 @@ Promise.all([
         .append("path")
         .attr("d", path)
         .attr("fill", "none")
-        .attr("stroke", "black")
-        .attr("stroke-width", 2)
-        .attr("width", width)  
-        .attr("height", height);  
-            
-
+        .attr("stroke", d => {
+            // Differentiate the stroke width based on the highway type
+            if (d.properties.highway === "residential") {
+                return "grey"; // Use a lighter color for residential roads if desired
+            } else {
+                return "black";
+            }
+        })
+        .attr("stroke-width", d => {
+            if (d.properties.highway === "residential") {
+                return 1; // Thinner stroke for residential roads
+            } else {
+                return 2; // Thicker stroke for primary and secondary roads
+            }
+        })
+        .attr("width", width)
+        .attr("height", height);
 
 });
