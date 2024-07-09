2dae382466106d4ef91fd86b5ed092c7315a07b4
Arta Seyedian <arta.seyedian@gmail.com>
2024-07-09 01:11:24 -0400

    wholesome roads

--- src/script.js
+++ src/script.js
@@ -1,51 +1,38 @@
-const width = 960;
-const height = 600;
+const width = 960*2;
+const height = 600*2;
 
-let currentSlide = 0;
-const slides = [
-    { title: "Overview", content: "" },
-    { title: "Fatality Hotspots", content: "" },
-    { title: "Time of Day Analysis", content: "" },
-    { title: "Vehicle Types Involved", content: "" },
-    { title: "Demographic Insights", content: "" }
-];
-
-// let phillySVG = d3.xml('/assets/Philadelphia (1).svg')
-let phillyMap = d3.json("/assets/geojson.json")
-// let phillyMap = d3.json("https://raw.githubusercontent.com/blackmad/neighborhoods/master/philadelphia.geojson")
+let phillyMap = d3.json("/assets/phillyRoads.json")
 let phillyCSV = d3.csv("/assets/philly_crashes.csv")
 
-// const boundingBox = phillyMap
-
 const projection = d3.geoMercator()
-    .center([-75.1652, 39.9526])  // Center on Philadelphia
+    .center([-75.1652, 39.9526]) 
     .scale(70000)
     .translate([width / 2, height / 1.6]);
 
 const path = d3.geoPath().projection(projection);
 
 Promise.all([
-    d3.xml('/assets/Philadelphia (1).svg'),
     phillyMap,
     d3.csv("/assets/philly_crashes.csv")
-]).then(([svgData, phillyMap, crashData]) => {
+]).then(([phillyMap, crashData]) => {
+
     const svg = d3.select("#visualization").append("svg")
         .attr("width", width)
         .attr("height", height);
 
-    // Append the loaded SVG to the container
-    const importedSVG = d3.select(svgData.documentElement);
-    svg.node().appendChild(importedSVG.node());
+    console.log(phillyMap)
 
-    // Draw the Philadelphia boundaries on top of the SVG
-    svg.selectAll(".boundary")
-        .data([phillyMap.geojson])
+    svg.selectAll("path")
+        .data(phillyMap.features)
         .enter()
         .append("path")
         .attr("d", path)
-        .attr("fill", "none")  // This makes it transparent
+        .attr("fill", "none")
         .attr("stroke", "black")
         .attr("stroke-width", 2)
-        .raise(); // Bring phillyMap to the front
+        .attr("width", width)  
+        .attr("height", height);  
+            
+
 
 });
