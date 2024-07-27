4b0b27839b47cc76e0c223b76f2498f891551646
Arta Seyedian <arta.seyedian@gmail.com>
2024-07-26 23:15:30 -0400

    added peds

--- src/script.js
+++ src/script.js
@@ -75,7 +75,7 @@ Promise.all([phillyMap, phillyCSV]).then(([phillyMap, fullCrashData]) => {
         .style("display", "none");
 
     collisionTypeFilter.selectAll("option")
-        .data(["All Collisions", "Fatalities", "Bicycle Fatalities"])
+        .data(["All Collisions", "Fatalities", "Bicycle Fatalities", "Pedestrian Fatalities"])
         .enter()
         .append("option")
         .text(d => d)
@@ -109,36 +109,58 @@ Promise.all([phillyMap, phillyCSV]).then(([phillyMap, fullCrashData]) => {
         tooltip.style("opacity", 0);
         textOverlay.html("");
 
-        sliderContainer.style("display", slideIndex === 4 ? "block" : "none");
-        collisionTypeFilter.style("display", slideIndex === 4 ? "block" : "none");
+        sliderContainer.style("display", slideIndex === 5 ? "block" : "none");
+        collisionTypeFilter.style("display", slideIndex === 5 ? "block" : "none");
 
         switch (slideIndex) {
             case 0: showIntroScene(); break;
             case 1: showAllCollisions(); break;
             case 2: showFatalities(); break;
             case 3: showBicycleFatalities(); break;
-            case 4: showTimeAnalysis(); break;
+            case 4: showPedestrianFatalities(); break;
+            case 5: showTimeAnalysis(); break;
         }
     }
 
     function showIntroScene() {
-        textOverlay.html(`
-            <h2>Philadelphia's Traffic Safety Crisis</h2>
+        const year = yearSelect.property("value");
+        const totalFatalities = crashData.reduce((sum, d) => sum + (+d.FATAL_COUNT), 0);
+        const pedestrianFatalities = crashData.reduce((sum, d) => sum + (+d.PED_DEATH_COUNT), 0);
+        const bicycleFatalities = crashData.reduce((sum, d) => sum + (+d.BICYCLE_DEATH_COUNT), 0);
+        const otherFatalities = totalFatalities - pedestrianFatalities - bicycleFatalities;
+
+        let introText = `
+            <h2>Philadelphia's Traffic Safety Crisis in ${year}</h2>
             <p>Philadelphia has one of the highest traffic death rates in the country, surpassing New York and Boston, and comparable to Los Angeles.</p>
-            <p>In 2023, Philadelphia saw 126 traffic fatalities, with pedestrians and cyclists accounting for more than half of these deaths.</p>
-            <p>Source: <a href="https://www.phillyvoice.com/philly-vision-zero-2023-report-traffic-deaths-vehicle-crashes-pedestrians/" target="_blank">PhillyVoice</a></p>
+            <p>In ${year}, Philadelphia saw ${totalFatalities} traffic fatalities:</p>
+            <ul>
+                <li>${pedestrianFatalities} were pedestrians</li>
+                <li>${bicycleFatalities} were bicyclists</li>
+                <li>${otherFatalities} were other traffic fatalities</li>
+            </ul>
+        `;
+
+        if (year === "2023") {
+            introText += `<p>Source: <a href="https://www.phillyvoice.com/philly-vision-zero-2023-report-traffic-deaths-vehicle-crashes-pedestrians/" target="_blank">PhillyVoice</a></p>`;
+        }
+
+        introText += `
             <br>
             <p>Click through the following scenes to explore Philadelphia's traffic collision data.</p>
+            <p>Use the year dropdown at the top right to change the dataset.</p>
             <p><strong>Click anywhere to begin your exploration.</strong></p>
-        `)
+        `;
+
+        textOverlay.html(introText)
             .transition()
             .duration(1000)
             .style("opacity", 1);
     }
 
     function showAllCollisions() {
+        const year = yearSelect.property("value");
         const collisionCount = crashData.length;
-        textOverlay.html(`<h2>In ${yearSelect.property("value")}, there were ${collisionCount} traffic collisions in Philadelphia</h2>`)
+        textOverlay.html(`<h2>In ${year}, there were ${collisionCount} traffic collisions in Philadelphia</h2>`)
             .transition()
             .duration(1000)
             .style("opacity", 1);
@@ -147,9 +169,10 @@ Promise.all([phillyMap, phillyCSV]).then(([phillyMap, fullCrashData]) => {
     }
 
     function showFatalities() {
+        const year = yearSelect.property("value");
         const fatalCrashes = crashData.filter(d => +d.FATAL_COUNT > 0);
-        const fatalityCount = fatalCrashes.length;
-        textOverlay.html(`<h2>Of these, ${fatalityCount} collisions resulted in fatalities</h2>`)
+        const fatalityCount = fatalCrashes.reduce((sum, d) => sum + (+d.FATAL_COUNT), 0);
+        textOverlay.html(`<h2>In ${year}, ${fatalityCount} people lost their lives in ${fatalCrashes.length} fatal collisions</h2>`)
             .transition()
             .duration(1000)
             .style("opacity", 1);
@@ -158,9 +181,10 @@ Promise.all([phillyMap, phillyCSV]).then(([phillyMap, fullCrashData]) => {
     }
 
     function showBicycleFatalities() {
+        const year = yearSelect.property("value");
         const bicycleFatalities = crashData.filter(d => +d.BICYCLE_DEATH_COUNT > 0);
-        const bicycleFatalityCount = bicycleFatalities.length;
-        textOverlay.html(`<h2>${bicycleFatalityCount} bicycle fatalities occurred in Philadelphia</h2>`)
+        const bicycleFatalityCount = bicycleFatalities.reduce((sum, d) => sum + (+d.BICYCLE_DEATH_COUNT), 0);
+        textOverlay.html(`<h2>In ${year}, ${bicycleFatalityCount} bicycle fatalities occurred in Philadelphia</h2>`)
             .transition()
             .duration(1000)
             .style("opacity", 1);
@@ -168,15 +192,28 @@ Promise.all([phillyMap, phillyCSV]).then(([phillyMap, fullCrashData]) => {
         createPoints(bicycleFatalities, "orange", 5, 0.8);
     }
 
-    function showTimeAnalysis() {
-        textOverlay.html(`
-            <h2>Analyze Crashes by Time of Day</h2>
-            <p>Use the slider below to explore how crashes occur throughout the day.</p>
-            <p>Change the collision type using the dropdown menu.</p>
-        `)
+    function showPedestrianFatalities() {
+        const year = yearSelect.property("value");
+        const pedestrianFatalities = crashData.filter(d => +d.PED_DEATH_COUNT > 0);
+        const pedestrianFatalityCount = pedestrianFatalities.reduce((sum, d) => sum + (+d.PED_DEATH_COUNT), 0);
+        textOverlay.html(`<h2>In ${year}, ${pedestrianFatalityCount} pedestrian fatalities occurred in Philadelphia</h2>`)
             .transition()
             .duration(1000)
             .style("opacity", 1);
+
+        createPoints(pedestrianFatalities, "purple", 5, 0.8);
+    }
+
+    function showTimeAnalysis() {
+        const year = yearSelect.property("value");
+        textOverlay.html(`
+            <h2>Analyze ${year} Crashes by Time of Day</h2>
+            <p>Use the slider below to explore how crashes occur throughout the day.</p>
+            <p>Change the collision type using the dropdown menu.</p>
+        `)
+        .transition()
+        .duration(1000)
+        .style("opacity", 1);
 
         updateVisualization();
     }
@@ -213,6 +250,7 @@ Promise.all([phillyMap, phillyCSV]).then(([phillyMap, fullCrashData]) => {
             Time: ${d.HOUR_OF_DAY}:00<br>
             Fatalities: ${d.FATAL_COUNT}<br>
             Bicycle Fatalities: ${d.BICYCLE_DEATH_COUNT}<br>
+            Pedestrian Fatalities: ${d.PED_DEATH_COUNT}<br>
             Injuries: ${d.INJURY_COUNT}`)
             .style("left", (event.pageX + 5) + "px")
             .style("top", (event.pageY - 28) + "px");
@@ -234,14 +272,17 @@ Promise.all([phillyMap, phillyCSV]).then(([phillyMap, fullCrashData]) => {
             filteredData = filteredData.filter(d => +d.FATAL_COUNT > 0);
         } else if (collisionType === "Bicycle Fatalities") {
             filteredData = filteredData.filter(d => +d.BICYCLE_DEATH_COUNT > 0);
+        } else if (collisionType === "Pedestrian Fatalities") {
+            filteredData = filteredData.filter(d => +d.PED_DEATH_COUNT > 0);
         }
 
         svg.selectAll(".crash-point").remove();
         createPoints(filteredData, d => {
             if (+d.BICYCLE_DEATH_COUNT > 0) return "orange";
+            if (+d.PED_DEATH_COUNT > 0) return "purple";
             if (+d.FATAL_COUNT > 0) return "red";
             return "blue";
-        }, 4, 0.6, true);  // Added 'true' for instant display
+        }, 4, 0.6, true);
 
         d3.select("#hourDisplay").text(`Hour: ${hour}`);
     }
@@ -249,7 +290,7 @@ Promise.all([phillyMap, phillyCSV]).then(([phillyMap, fullCrashData]) => {
     // Event listeners
     d3.select("#visualization").on("click", (event) => {
         if (event.target.tagName !== "SELECT" && event.target.tagName !== "INPUT") {
-            currentSlide = (currentSlide + 1) % 5;
+            currentSlide = (currentSlide + 1) % 6;  // Changed to 6 for the new total number of slides
             showSlide(currentSlide);
         }
     });
@@ -273,7 +314,7 @@ Promise.all([phillyMap, phillyCSV]).then(([phillyMap, fullCrashData]) => {
         .text(d => d)
         .attr("value", d => d);
 
-    yearSelect.on("change", function () {
+    yearSelect.on("change", function() {
         crashData = filterDataByYear(this.value);
         showSlide(currentSlide);
     });
